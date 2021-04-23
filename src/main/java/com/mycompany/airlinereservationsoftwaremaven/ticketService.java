810d646da5684e481da99b7a6d5f4f61b1fe8480
cmartinez0818 <54378633+cmartinez0818@users.noreply.github.com>
2021-04-22 23:24:40 -0400

    Update ticketService.java
    comments

--- src/main/java/com/mycompany/airlinereservationsoftwaremaven/ticketService.java
+++ src/main/java/com/mycompany/airlinereservationsoftwaremaven/ticketService.java
@@ -1,9 +1,10 @@
 package com.mycompany.airlinereservationsoftwaremaven;
 
 public interface ticketService {
+  //  call update for tickets in test
   public void execUpdateCalled();
-
+// show message 
   public String showRegistrationUpdatedMsg(String msg);
-
+// show not found
   public void showCustNotFoundMsg();
 }
